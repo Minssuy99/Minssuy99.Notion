773cd1475b896eed80ea2e2febdfdf82b189030e
Minssuy <rlaalstjd6302@naver.com>
2025-09-29 00:16:20 +0900

    [2025-09-28] Update

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Minssuy99",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Game Developer",
-    bio: "Study and Learn for Game Development.",
+    bio: "Study Game Development.",
     email: "mkm6302@gmail.com",
     linkedin: "",
     github: "minssuy99",
