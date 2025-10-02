2f050378d3042921d3940a687e1d0d6984d31886
Minssuy <rlaalstjd6302@naver.com>
2025-10-03 04:07:38 +0900

    [2025-10-02] Update

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -241,7 +241,10 @@ const StyledWrapper = styled.div<StyledWrapperProps>`
   }
 
   .notion-hr {
-    border-top: 1px solid rgba(55, 53, 47, 0.16);
+    border: none; /* 기존 border-top을 초기화 */
+    border-top: 2px solid ${({ theme }) =>
+      theme === "dark" ? "rgba(255, 255, 255, 0.3)" : "rgba(0, 0, 0, 0.2)"};
+    margin: 0.7rem 0; /* 위아래 여백 조절 */
   }
 
   .notion-table-of-contents {
